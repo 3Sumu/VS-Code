202e562f6074a5e9113a9da1cdb8ae5041dcf67c
3Sumu <sumanmidya96@gmail.com>
2022-02-20 21:13:31 +0530

    updates

--- AccioJob/HTML/Assignment/Drag n Drop/script.js
+++ AccioJob/HTML/Assignment/Drag n Drop/script.js
@@ -1,46 +1,70 @@
-const list_items = document.querySelectorAll(".list_item");
-const lists = document.querySelectorAll(".list");
+const mainDiv = document.querySelector(".newtask");
 
-let draggedItem = null;
+document.getElementById("submit").addEventListener("click", (e) => {
+  e.preventDefault();
+  const taskName = document.getElementById("input");
 
-for (let i = 0; i < list_items.length; i++) {
-  const item = list_items[i];
+  if (taskName.value) {
+    const siblingDiv = document.createElement("div");
+    siblingDiv.classList.add("list_item");
+    siblingDiv.setAttribute("draggable", "true");
+    siblingDiv.innerText = taskName.value;
 
-  item.addEventListener("dragstart", (e) => {
-    console.log("dragstrt");
-    draggedItem = item;
-    setTimeout(() => {
-      item.style.display = "none";
-    }, 0);
-  });
+    mainDiv.appendChild(siblingDiv);
+    taskName.value = "";
+    addEvent();
+  } else alert("Enter Task Name");
+});
 
-  item.addEventListener("dragend", () => {
-    console.log("dragend");
-    setTimeout(() => {
-      draggedItem.style.display = "block";
-      draggedItem = null;
-    }, 0);
-  });
+function addEvent() {
+  const list_items = document.querySelectorAll(".list_item");
+  const lists = document.querySelectorAll(".list");
+  let draggedItem = "";
 
-  for (let j = 0; j < lists.length; j++) {
-    const list = lists[j];
+  for (let i = 0; i < list_items.length; i++) {
+    const item = list_items[i];
 
-    list.addEventListener("dragover", (e) => {
+    item.addEventListener("dragstart", (e) => {
+      console.log("dragstrt");
+      draggedItem = item;
+      setTimeout(() => {
+        item.style.display = "none";
+      }, 0);
+    });
+
+    item.addEventListener("dragend", () => {
+      console.log("dragend");
+      setTimeout(() => {
+        draggedItem.style.display = "block";
+        draggedItem = "";
+      }, 0);
+    });
+
+    item.addEventListener("contextmenu", (e) => {
       e.preventDefault();
+      item.remove();
     });
 
-    list.addEventListener("dragenter", function (e) {
-      e.preventDefault();
-      list.style.backgroundColor = "rgba(208, 228, 26,0.5)";
-    });
+    for (let j = 0; j < lists.length; j++) {
+      const list = lists[j];
 
-    list.addEventListener("dragleave", function (e) {
-      list.style.backgroundColor = "rgba(208, 228, 26,1)";
-    });
+      list.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
 
-    list.addEventListener("drop", function (e) {
-      list.append(draggedItem);
-      list.style.backgroundColor = "rgba(208, 228, 26,1)";
-    });
+      list.addEventListener("dragenter", function (e) {
+        e.preventDefault();
+        list.style.backgroundColor = "rgba(208, 228, 26,0.5)";
+      });
+
+      list.addEventListener("dragleave", function (e) {
+        list.style.backgroundColor = "rgba(208, 228, 26,1)";
+      });
+
+      list.addEventListener("drop", function (e) {
+        list.append(draggedItem);
+        list.style.backgroundColor = "rgba(208, 228, 26,1)";
+      });
+    }
   }
 }
