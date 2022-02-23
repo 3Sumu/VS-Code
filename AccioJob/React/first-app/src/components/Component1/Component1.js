5e9acfd120ce2e8267f34c658bccb69eb2de80f4
3Sumu <sumanmidya96@gmail.com>
2022-02-23 12:34:33 +0530

    Chnage

---
+++ AccioJob/React/first-app/src/components/Component1/Component1.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Developer from "./Developer/Developer";
+import "./Component1.css";
+
+function Component1() {
+  return (
+    <div className="Component1">
+      <h1>Heading1</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit a ut
+        impedit placeat, dolore, ex, quod voluptatem fuga doloremque id maiores
+        esse odit sint. Alias laudantium accusantium sed modi, nihil fuga iusto
+        quos qui numquam iste minima corporis quas facere accusamus provident
+        facilis ducimus, optio tempore eveniet culpa animi vel in! Aperiam
+        eligendi necessitatibus labore, impedit delectus harum perspiciatis
+        nulla accusantium corrupti! Reiciendis fuga velit minima ad dolores,
+        quis nihil placeat voluptatibus! A nobis veniam dolor sint. Aut,
+        temporibus unde!
+      </p>
+      <Developer />
+    </div>
+  );
+}
+
+export default Component1;
