ad6e11d8361c0faa8e513b3ea45ac7a7d393375a
3Sumu <sumanmidya96@gmail.com>
2022-03-22 12:21:55 +0530

    new project

--- AccioJob/React/portfolio-demo/src/App.js
+++ AccioJob/React/portfolio-demo/src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+// import "./App.css";
+import ReactMemo from "./ReactMemo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ReactMemo />
+      <h1 class="animate__animated animate__lightSpeedInRight">
+        An animated element
+      </h1>
+    </div>
+  );
 }
 
 export default App;
